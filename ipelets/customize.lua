eb5b2fb66454275952bf8aebfda0282b7d7e8704
ThiiD <thiago.saber@engenharia.ufjf.br>
2022-02-11 15:25:55 -0300

    Inclusao de arquivos com simbologia americana

--- ipelets/customize.lua
+++ ipelets/customize.lua
@@ -4,16 +4,8 @@
 prefs.max_zoom = 100
 shortcuts.ipelet_1_symbols = "Shift+S"
 shortcuts.mode_splines = "Alt+Ctrl+I"
+shortcuts.ipelet_1_automatic_labelling = "Ctrl+D"
+shortcuts.ipelet_2_automatic_labelling = "Ctrl+Alt+D"
 prefs.scroll.vertical_sign = -1
-
-shortcuts["horizontalalignment|left"] = "Alt+Shift+L"
-shortcuts["horizontalalignment|hcenter"] = "Alt+Shift+H"
-shortcuts["horizontalalignment|right"] = "Alt+Shift+R"
-
-shortcuts["verticalalignment|bottom"] = "Alt+Shift+B"
-shortcuts["verticalalignment|baseline"] = "Alt+B"
-shortcuts["verticalalignment|vcenter"] = "Alt+Shift+C"
-shortcuts["verticalalignment|top"] = "Alt+Shift+T"
-
--- Substitute with absolute /path/to/ElectricalEngineering.isy
-prefs.styles = { "~/.ipe/styles/ElectricalEngineering.isy" }
+-- Substitute with /path/to/ElectricalEngineering.isy
+prefs.styles = { "C:\\Users\\Administrador\\Desktop\\ipe-7.2.24\\styles\\ElectricalEngineering.isy" }
